ba22068d4deb0cf7992e169c54c21e680c1035d3
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-03-13 21:47:35 +0100

    clean up folder structure

--- Kosebloggen/articles/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Author, Article, Rating
-
-admin.site.register(Article)
-admin.site.register(Author)
-admin.site.register(Rating)
--- Kosebloggen/articles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'articles'
-urlpatterns = [
-    path('', views.ArticleListView.as_view(), name='list'),
-    path('<int:pk>/', views.ArticleDetailView.as_view(), name='detail'),
-]
